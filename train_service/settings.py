038aef17d6af6db5befcf21af2f01e951f85b97a
Artem-user-19 <temakot254@gmail.com>
2024-09-28 13:13:38 +0300

    Solution

--- train_service/settings.py
+++ train_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-23=^pe1vpux9=2="
-              "zfbx6rz8(qymgi3#@o*($&mnd3o7eige2hc")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
